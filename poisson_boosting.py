c87633df34852082e6adfce37755ab3f8aa388de
madrury <matthew.drury.83@gmail.com>
2018-01-08 13:00:31 -0800

    Maybe this works?

--- poisson_boosting.py
+++ poisson_boosting.py
@@ -1,7 +1,10 @@
 import numpy as np
+from sklearn.base import clone
+from sklearn.tree import DecisionTreeRegressor
 
 
 class GradientBoostingPoissonRegressor:
+
 
     def __init__(self, 
                  n_estimators=100,
@@ -14,29 +17,56 @@ class GradientBoostingPoissonRegressor:
         self.tree_depth = tree_depth
         self.subsample = subsample
         self.weak_learner = DecisionTreeRegressor(max_depth=self.max_depth)
+        
         self.initial_value = None
         self.estimators = []
+        self.terminal_node_estimates = []
 
     def fit(X, y):
         self.initial_value = self._initial_value(y)
         working_prediction = np.repeat(self.initial_value, y.shape[0])
-        working_response = y - np.exp(working_prediction)
+        working_response = self._gradient(y, working_prediction)
 
         for _ in range(self.n_estimators):
-            pass
+            tree = clone(self.weak_learner).fit(X, working_response)
+            terminal_node_estimates = self._terminal_node_estimates(
+                y, working_prediction, terminal_node_mask)
+            prediction_update = self._prediction_update(
+                X, tree, terminal_node_estimates)
+            working_prediction += terminal_node_estimates
+            working_response = self._gradient(y, working_prediction)
 
+            self.estimators.append(tree)
+            self.terminal_node_estimates[idx, :] = terminal_node_estimates
 
-    def predict(X):
-        pass
-
+    def predict(self, X):
+        y_hat = np.repeat(self.initial_value)
+        for tree, tn_estimates in zip(self.estimators, terminal_node_estimates):
+            y_hat += self._prediction_update(X, tree, tn_estimates)
+        return np.exp(y_hat)
+            
     def _initial_value(self, y):
-        pass
+        return np.log(np.mean(y))
 
     def _gradient(self, y, y_hat):
-        pass
+        return y - np.exp(y_hat)
 
-    def _get_terminal_nodes(self, X, tree):
-        pass
+    def _terminal_node_estimates(self, X, y, y_hat, tree):
+        exp_y_hat = np.exp(y_hat)
 
-    def _terminal_node_estimates(self, y, y_hat, terminal_node_mask):
-        pass
+        estiamtes = {}
+        terminal_node_assignments = tree.apply(X)
+        terminal_node_idxs = np.unique(terminal_node_assignments)
+        for idx in terminal_node_idxs:
+            in_terminal_node = (terminal_node_assignments == idx)
+            y_in_node = np.sum(y[in_terminal_node])
+            exp_y_hat_in_node = np.sum(exp_y_hat[in_terminal_node])
+            estiamtes[idx] = np.log(y_in_node / exp_y_hat_in_node)
+        return estiamtes
+
+    def _prediction_update(self, X, tree, terminal_node_estimates):
+        terminal_node_assignments = tree.apply(X)
+        for idx in terminal_node_estimates:
+            in_terminal_node = (terminal_node_assignments == idx)
+            prediction_update[in_terminal_node] = terminal_node_estimates[idx]
+        return prediction_update
