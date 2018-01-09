bac11aea76f5563c5265ddc015c38bbae5134964
madrury <matthew.drury.83@gmail.com>
2018-01-08 17:09:08 -0800

    Finish poisson boosting prototype.

--- poisson_boosting.py
+++ poisson_boosting.py
@@ -8,41 +8,43 @@ class GradientBoostingPoissonRegressor:
 
     def __init__(self, 
                  n_estimators=100,
-                 learing_rate=0.01,
-                 tree_depth=2,
-                 subsample=0.5):
+                 learning_rate=0.001,
+                 max_depth=2):
         
         self.n_estimators = n_estimators
-        self.learing_rate = learing_rate
-        self.tree_depth = tree_depth
-        self.subsample = subsample
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
         self.weak_learner = DecisionTreeRegressor(max_depth=self.max_depth)
         
         self.initial_value = None
         self.estimators = []
         self.terminal_node_estimates = []
 
-    def fit(X, y):
+    def fit(self, X, y):
         self.initial_value = self._initial_value(y)
         working_prediction = np.repeat(self.initial_value, y.shape[0])
         working_response = self._gradient(y, working_prediction)
 
-        for _ in range(self.n_estimators):
+        for idx in range(self.n_estimators):
+
             tree = clone(self.weak_learner).fit(X, working_response)
             terminal_node_estimates = self._terminal_node_estimates(
-                y, working_prediction, terminal_node_mask)
+                X, y, working_prediction, tree)
             prediction_update = self._prediction_update(
                 X, tree, terminal_node_estimates)
-            working_prediction += terminal_node_estimates
+
+            working_prediction += self.learning_rate * prediction_update
             working_response = self._gradient(y, working_prediction)
 
             self.estimators.append(tree)
-            self.terminal_node_estimates[idx, :] = terminal_node_estimates
+            self.terminal_node_estimates.append(terminal_node_estimates)
+        return self
 
     def predict(self, X):
-        y_hat = np.repeat(self.initial_value)
-        for tree, tn_estimates in zip(self.estimators, terminal_node_estimates):
-            y_hat += self._prediction_update(X, tree, tn_estimates)
+        y_hat = np.repeat(self.initial_value, X.shape[0])
+        estimator_data = zip(self.estimators, self.terminal_node_estimates)
+        for tree, tn_estimates in estimator_data:
+            y_hat += self.learning_rate * self._prediction_update(X, tree, tn_estimates)
         return np.exp(y_hat)
             
     def _initial_value(self, y):
@@ -54,17 +56,18 @@ class GradientBoostingPoissonRegressor:
     def _terminal_node_estimates(self, X, y, y_hat, tree):
         exp_y_hat = np.exp(y_hat)
 
-        estiamtes = {}
+        estimates = {}
         terminal_node_assignments = tree.apply(X)
         terminal_node_idxs = np.unique(terminal_node_assignments)
         for idx in terminal_node_idxs:
             in_terminal_node = (terminal_node_assignments == idx)
             y_in_node = np.sum(y[in_terminal_node])
             exp_y_hat_in_node = np.sum(exp_y_hat[in_terminal_node])
-            estiamtes[idx] = np.log(y_in_node / exp_y_hat_in_node)
-        return estiamtes
+            estimates[idx] = np.log(y_in_node / exp_y_hat_in_node)
+        return estimates
 
     def _prediction_update(self, X, tree, terminal_node_estimates):
+        prediction_update = np.zeros(X.shape[0])
         terminal_node_assignments = tree.apply(X)
         for idx in terminal_node_estimates:
             in_terminal_node = (terminal_node_assignments == idx)
