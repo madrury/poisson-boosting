9c354e55b5b91f477f35275890c88a32db76721a
madrury <matthew.drury.83@gmail.com>
2018-01-11 15:14:52 -0800

    Rename some varaibles.

--- poisson_boosting.py
+++ poisson_boosting.py
@@ -16,7 +16,6 @@ class GradientBoostingPoissonRegressor:
 
     Parameters
     ----------
-
     n_estimators: int
         A positive integer.  The number of regression trees to stack when
         estimating f.
@@ -30,7 +29,6 @@ class GradientBoostingPoissonRegressor:
 
     Attributes
     ----------
-
     n_estimators: int
         The number of regression trees to stack when estimating f.
 
@@ -58,7 +56,6 @@ class GradientBoostingPoissonRegressor:
                  n_estimators=100,
                  learning_rate=0.001,
                  max_depth=2):
-        
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
         self.max_depth = max_depth
@@ -69,6 +66,16 @@ class GradientBoostingPoissonRegressor:
         self.terminal_node_estimates_ = []
 
     def fit(self, X, y):
+        """Fit boosted model to training data.
+
+        Parameters
+        ----------
+        X: array, shape (n_samples, n_features)
+            Training data.
+
+        y: array, shape (n_samples, )
+            Target values.
+        """
         self.initial_value_ = self._initial_value(y)
         working_prediction = np.repeat(self.initial_value_, y.shape[0])
         working_response = self._gradient(y, working_prediction)
@@ -85,28 +92,37 @@ class GradientBoostingPoissonRegressor:
         return self
 
     def predict(self, X):
-        y_hat = np.repeat(self.initial_value_, X.shape[0])
+        """Return predictions from a fit model.
+
+        Parameters
+        ----------
+        X: array, shape (n_samples, n_features)
+            Data set.
+        """
+        log_prediction = np.repeat(self.initial_value_, X.shape[0])
         estimator_data = zip(self.estimators_, self.terminal_node_estimates_)
         for tree, tn_estimates in estimator_data:
-            y_hat += self.learning_rate * self._prediction_update(X, tree, tn_estimates)
-        return np.exp(y_hat)
+            log_prediction += (
+                self.learning_rate * 
+                self._prediction_update(X, tree, tn_estimates))
+        return np.exp(log_prediction)
             
     def _initial_value(self, y):
         return np.log(np.mean(y))
 
-    def _gradient(self, y, y_hat):
-        return y - np.exp(y_hat)
+    def _gradient(self, y, log_prediction):
+        return y - np.exp(log_prediction)
 
-    def _terminal_node_estimates(self, X, y, y_hat, tree):
-        exp_y_hat = np.exp(y_hat)
+    def _terminal_node_estimates(self, X, y, log_prediction, tree):
+        prediction = np.exp(log_prediction)
         estimates = {}
         terminal_node_assignments = tree.apply(X)
         terminal_node_idxs = np.unique(terminal_node_assignments)
         for idx in terminal_node_idxs:
             in_terminal_node = (terminal_node_assignments == idx)
             y_in_node = np.sum(y[in_terminal_node])
-            exp_y_hat_in_node = np.sum(exp_y_hat[in_terminal_node])
-            estimates[idx] = np.log(y_in_node / exp_y_hat_in_node)
+            prediction_in_node = np.sum(prediction[in_terminal_node])
+            estimates[idx] = np.log(y_in_node / prediction_in_node)
         return estimates
 
     def _prediction_update(self, X, tree, terminal_node_estimates):
